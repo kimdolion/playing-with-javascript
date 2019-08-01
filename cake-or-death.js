a07c6449ee1b0f787da492f5f4487939682cf427
kimdolion <kimowilkes@gmail.com>
2019-08-01 10:51:13 -0400

    Add prompt node

--- cake-or-death.js
+++ cake-or-death.js
@@ -4,9 +4,18 @@ const cakeDeath = [
   'cake',
   'death'
 ]
-const yourChoice = 1
-console.log('You must choose: cake or death?')
-
-const randomCakeDeath = cakeDeath[Math.floor(Math.random() * cakeDeath.length)]
-
-console.log('Computer picked: ' + randomCakeDeath + '. Hope you enjoy ' + randomCakeDeath + '! Them\'s the rules.')
+let yourChoice = prompt( 'You must choose: 1 for cake or 2 for death?' )
+console.log( yourChoice )
+while (yourChoice != 1 && yourChoice != 2) {
+  console.log('You must choose cake (1) or death (2)!')
+  console.log(yourChoice)
+  if (yourChoice === 1) {
+    console.log('You chose cake!')
+  } else if (yourChoice === 2) {
+    console.log('You chose death!')
+  }
+}
+while (yourChoice === 1 || yourChoice === 2){
+  const randomCakeDeath = cakeDeath[Math.floor(Math.random() * cakeDeath.length)]
+  console.log('Computer decided you get: ' + randomCakeDeath + '. Hope you enjoy ' + randomCakeDeath + '! Them\'s the rules.')
+}
