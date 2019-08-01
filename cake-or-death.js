2e4e0ba28fc285d589eccd1a77a422cdc2487fd7
kimdolion <kimowilkes@gmail.com>
2019-08-01 11:06:59 -0400

    Add loop and if statements
    Removed possible infinite loop in previous commit.
    Added if else statements to print out what the choice was.
    Added a loop to make your fate be decided by three computers.

--- cake-or-death.js
+++ cake-or-death.js
@@ -4,18 +4,19 @@ const cakeDeath = [
   'cake',
   'death'
 ]
-let yourChoice = prompt( 'You must choose: 1 for cake or 2 for death?' )
+console.log( 'You must choose: 1 for cake or 2 for death. Make your choice.' )
+const yourChoice = 1
 console.log( yourChoice )
-while (yourChoice != 1 && yourChoice != 2) {
-  console.log('You must choose cake (1) or death (2)!')
-  console.log(yourChoice)
+if (yourChoice === 1 || yourChoice === 2) {
   if (yourChoice === 1) {
     console.log('You chose cake!')
   } else if (yourChoice === 2) {
     console.log('You chose death!')
+  } else {
+    console.log('You didn\'t choose either! Pick cake or death!')
   }
 }
-while (yourChoice === 1 || yourChoice === 2){
+for (let i = 1; i <= 3; i++) {
   const randomCakeDeath = cakeDeath[Math.floor(Math.random() * cakeDeath.length)]
-  console.log('Computer decided you get: ' + randomCakeDeath + '. Hope you enjoy ' + randomCakeDeath + '! Them\'s the rules.')
+  console.log('Computer ' + i + ' decided you get: ' + randomCakeDeath + '. Hope you enjoy ' + randomCakeDeath + '! Them\'s the rules.')
 }
